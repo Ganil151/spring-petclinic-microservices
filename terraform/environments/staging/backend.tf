d797c36640e558c025dc50d4d4040bcf4cb4e0e5
Ganil151 <ganilbatistyan@gmail.com>
2026-02-10 14:06:36 -0400

    refactor(spring-petclinic-microservices): Update backend.tf
    Changes summary:
    - Modified: 1 files
      • terraform/environments/staging/backend.tf

--- terraform/environments/staging/backend.tf
+++ terraform/environments/staging/backend.tf
@@ -1,10 +1,10 @@
-  # S3 Backend configuration for remote state management
+# S3 Backend configuration for remote state management
 # terraform {
 #   backend "s3" {
 #     bucket         = var.bucket_name
 #     key            = "tfstate/staging/terraform.tfstate"
 #     region         = var.aws_region
-#     dynamodb_table = "petclinic-terraform-locks"
+#     dynamodb_table = var.dynamodb_table
 #     encrypt        = true
 #   }
 # }
