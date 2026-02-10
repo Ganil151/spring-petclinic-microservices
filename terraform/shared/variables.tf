6cbb2529bf4356c28b4c8ff4802fd9e9bac7f17e
Ganil151 <ganilbatistyan@gmail.com>
2026-02-10 06:11:29 -0400

    refactor(spring-petclinic-microservices): Update variables.tf
    Changes summary:
    - Modified: 1 files
      • terraform/shared/variables.tf

--- terraform/shared/variables.tf
+++ terraform/shared/variables.tf
@@ -1,7 +1,30 @@
 variable "aws_region" {}
 variable "Environment" {}
+variable "aws_region" {
+  type        = string
+  description = "The AWS region to deploy resources into"
+}
+
+variable "Environment" {
+  type        = string
+  description = "The deployment environment (e.g., dev, prod, staging)"
+}
+
 variable "bucket_name" {
   default = "petclinic-terraform-state-17a538b3"
+  type        = string
+  description = "The name of the S3 bucket for Terraform state"
+  default     = "petclinic-terraform-state-17a538b3"
 }
 variable "spms_vpc_cidr" {}
 variable "spms_subnet_cidr" {}
+
+variable "spms_vpc_cidr" {
+  type        = string
+  description = "The CIDR block for the VPC"
+}
+
+variable "spms_subnet_cidr" {
+  type        = string
+  description = "The CIDR block for the subnet"
+}
