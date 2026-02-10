8e740483b5d9a280eb9cfe54a9cdc78e566baa75
Ganil151 <ganilbatistyan@gmail.com>
2026-02-10 17:03:38 -0400

    feat(spring-petclinic-microservices): Add keypair.tf
    Changes summary:
    - Untracked: 1 files
      • terraform/environments/dev/keypair.tf

---
+++ terraform/environments/dev/keypair.tf
@@ -0,0 +1,10 @@
+resource "aws_key_pair" "spms_pro" {
+  key_name   = "spms-pro"
+  public_key = file("${path.module}/../../keys/spms-pro.pub")
+
+  tags = {
+    Name        = "spms-pro"
+    Environment = var.environment
+    Project     = var.project_name
+  }
+}
