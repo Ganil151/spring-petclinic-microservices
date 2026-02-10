d0d4a40c60b220ae3b3fa232d8f8c2f5560ae9d7
Ganil151 <ganilbatistyan@gmail.com>
2026-02-10 16:14:48 -0400

    feat(spring-petclinic-microservices): Add output.tf and 1 others
    Changes summary:
    - Deleted: 1 files
      • terraform/modules/compute/eks/output.tf
    - Untracked: 1 files
      • terraform/modules/compute/eks/outputs.tf

---
+++ terraform/modules/compute/eks/outputs.tf
@@ -0,0 +1,24 @@
+output "cluster_name" {
+  description = "The name of the EKS cluster"
+  value       = aws_eks_cluster.this.name
+}
+
+output "cluster_endpoint" {
+  description = "The endpoint for the EKS cluster"
+  value       = aws_eks_cluster.this.endpoint
+}
+
+output "cluster_certificate_authority_data" {
+  description = "The certificate-authority-data for the EKS cluster"
+  value       = aws_eks_cluster.this.certificate_authority[0].data
+}
+
+output "node_group_id" {
+  description = "The ID of the EKS node group"
+  value       = aws_eks_node_group.this.id
+}
+
+output "node_role_arn" {
+  description = "The ARN of the IAM role used by the EKS nodes"
+  value       = aws_iam_role.nodes.arn
+}
