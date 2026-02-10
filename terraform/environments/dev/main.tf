1b05012feeb1651d12bed2925e9939e3c11aec7b
Ganil151 <ganilbatistyan@gmail.com>
2026-02-10 15:12:30 -0400

    refactor(spring-petclinic-microservices): Update main.tf
    Changes summary:
    - Modified: 1 files
      • terraform/environments/dev/main.tf

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -9,4 +9,21 @@ module "vpc" {
   availability_zones   = var.data_availability_zone
 }
 
-module
+module "ec2" {
+  source = "../../modules/ec2"
+
+  project_name = var.project_name
+  environment  = var.environment
+  instance_name  = var.instance_name
+  ami_id         = var.ami_id
+  instance_type  = var.instance_type
+  vpc_id         = module.vpc.id
+  subnet_id      = module.vpc.private_subnet_ids[0]
+  key_name       = var.key_name
+  associate_public_ip = var.associate_public_ip
+  user_data      = var.user_data
+  iam_instance_profile = var.iam_instance_profile
+  root_volume_size = var.root_volume_size
+  allowed_cidr_blocks = var.allowed_cidr_blocks
+  extra_volume_size = var.extra_volume_size
+} 
