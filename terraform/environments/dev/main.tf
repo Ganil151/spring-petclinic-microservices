ad84e126669b8f8c6ab730bd341a49996edea775
Ganil151 <ganilbatistyan@gmail.com>
2026-02-10 13:01:42 -0400

    refactor(spring-petclinic-microservices): Update main.tf
    Changes summary:
    - Modified: 1 files
      • terraform/environments/dev/main.tf

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -1,10 +1,10 @@
 module "vpc" {
   source = "../../modules/vpc"
 
-  project_name        = var.project_name
-  environment         = var.environment
-  vpc_cidr            = var.vpc_cidr
-  public_subnet_cidrs = var.public_subnet_cidrs
+  project_name         = var.project_name
+  environment          = var.environment
+  vpc_cidr             = var.vpc_cidr
+  public_subnet_cidrs  = var.public_subnet_cidrs
   private_subnet_cidrs = var.private_subnet_cidrs
-  availability_zones  = var.data_availability_zone
+  availability_zones   = var.data_availability_zone
 }
