5de6900d10891d10f87e9e97b15d31eb9d276904
Ganil151 <ganilbatistyan@gmail.com>
2026-02-10 10:50:27 -0400

    refactor(spring-petclinic-microservices): Update versions.tf
    Changes summary:
    - Modified: 1 files
      • terraform/environments/dev/versions.tf

--- terraform/environments/dev/versions.tf
+++ terraform/environments/dev/versions.tf
@@ -1,12 +1,12 @@
 terraform {
   required_version = ">= 1.6.0"
   required_providers {
-    aws = { 
-      source = "hashicorp/aws"
+    aws = {
+      source  = "hashicorp/aws"
       version = "~> 6.0"
     }
     random = {
-      source = "hashicorp/random"
+      source  = "hashicorp/random"
       version = "5.0"
     }
   }
