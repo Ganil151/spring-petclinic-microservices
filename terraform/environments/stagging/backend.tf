dc397fd25b5a3a92f89f322c9c81b06b37f23737
Ganil151 <ganilbatistyan@gmail.com>
2026-02-10 09:08:02 -0400

    refactor(spring-petclinic-microservices): Update backend.tf and 42 others
    Changes summary:
    - Deleted: 43 files
      • terraform/environments/dev/backend.tf
      • terraform/environments/dev/main.tf
      • terraform/environments/dev/outputs.tf
      • ... and 40 more

--- terraform/environments/stagging/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket  = "petclinic-terraform-state-17a538b3"
-    key     = "staging/terraform.tfstate"
-    region  = "us-east-1"
-    encrypt = true
-  }
-}
--- terraform/modules/rds/output.tf
+++
@@ -1,7 +0,0 @@
-output "db_instance_endpoint" {
-  value = aws_db_instance.main.endpoint
-}
-
-output "db_instance_address" {
-  value = aws_db_instance.main.address
-}
