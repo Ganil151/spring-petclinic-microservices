8fa4ba6c8c88fded784ca79493d0773bfb859321
Ganil151 <ganilbatistyan@gmail.com>
2026-02-10 16:13:56 -0400

    feat(spring-petclinic-microservices): Add main.tf and 1 others
    Changes summary:
    - Untracked: 2 files
      • terraform/modules/database/rds/main.tf
      • terraform/modules/database/rds/outputs.tf

---
+++ terraform/modules/database/rds/outputs.tf
@@ -0,0 +1,19 @@
+output "rds_endpoint" {
+  description = "The connection endpoint for the RDS instance"
+  value       = aws_db_instance.this.endpoint
+}
+
+output "rds_address" {
+  description = "The address of the RDS instance"
+  value       = aws_db_instance.this.address
+}
+
+output "rds_port" {
+  description = "The port of the RDS instance"
+  value       = aws_db_instance.this.port
+}
+
+output "rds_security_group_id" {
+  description = "The ID of the security group for the RDS instance"
+  value       = aws_security_group.rds.id
+}
