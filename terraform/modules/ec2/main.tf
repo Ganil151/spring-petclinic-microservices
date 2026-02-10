a4b88d1371798ad731fc030dc2b9ee7450cd00ea
Ganil151 <ganilbatistyan@gmail.com>
2026-02-10 08:49:08 -0400

    refactor(spring-petclinic-microservices): Update main.tf and 2 others
    Changes summary:
    - Modified: 3 files
      • terraform/modules/ec2/main.tf
      • terraform/modules/ec2/output.tf
      • terraform/modules/ec2/variables.tf

---
+++ terraform/modules/ec2/main.tf
@@ -0,0 +1,44 @@
+resource "aws_security_group" "bastion" {
+  name        = "${var.instance_name}-sg"
+  description = "Allow SSH access"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # In real life, restrict this to your IP
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.instance_name}-sg"
+  }
+}
+
+data "aws_ami" "amazon_linux_2" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
+
+resource "aws_instance" "bastion" {
+  ami                    = data.aws_ami.amazon_linux_2.id
+  instance_type          = var.instance_type
+  subnet_id              = var.public_subnet_id
+  vpc_security_group_ids = [aws_security_group.bastion.id]
+
+  tags = {
+    Name = var.instance_name
+  }
+}
