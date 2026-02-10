ecdded04d0a8e87c1c88dd13ab86ba8b5c82c759
Ganil151 <ganilbatistyan@gmail.com>
2026-02-10 07:41:10 -0400

    refactor(spring-petclinic-microservices): Update main.tf and 1 others
    Changes summary:
    - Modified: 2 files
      • terraform/environments/prod/main.tf
      • terraform/environments/stagging/main.tf

--- terraform/environments/prod/main.tf
+++ terraform/environments/prod/main.tf
@@ -1,7 +1,9 @@
 module "networking" {
   source = "../../modules/networking"
 
-  vpc_cidr    = var.spms_vpc_cidr
-  subnet_cidr = var.spms_subnet_cidr
-  environment = var.Environment
+  vpc_cidr             = var.spms_vpc_cidr
+  public_subnet_cidrs  = var.spms_public_subnet_cidrs
+  private_subnet_cidrs = var.spms_private_subnet_cidrs
+  availability_zones   = var.availability_zones
+  environment          = var.Environment
 }
