eee4e3cb5c1cd385e55c23e6aaf0684f5f51024a
Ganil151 <ganilbatistyan@gmail.com>
2026-02-10 10:36:29 -0400

    refactor(spring-petclinic-microservices): Update variables.tf
    Changes summary:
    - Modified: 1 files
      • terraform/environments/dev/variables.tf

---
+++ terraform/environments/dev/variables.tf
@@ -0,0 +1,59 @@
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "environment" {
+  description = "Environment name"
+  type        = string
+  default     = "dev"
+}
+
+variable "vpc_cidr" {
+  description = "VPC CIDR block"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnets_cidrs" {
+  description = "Public subnets CIDR blocks"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "private_subnets_cidrs" {
+  description = "Private subnets CIDR blocks"
+  type        = list(string)
+  default     = ["10.0.10.0/24", "10.0.11.0/24"]
+}
+
+variable "availability_zones" {
+  description = "Availability zones"
+  type        = list(string)
+  default     = ["us-east-1a", "us-east-1b"]
+}
+
+variable "cluster_version" {
+  description = "EKS Cluster version"
+  type        = string
+  default     = "1.31"
+}
+
+variable "db_instance_class" {
+  description = "RDS instance class"
+  type        = string
+  default     = "db.t3.micro"
+}
+
+variable "db_allocated_storage" {
+  description = "RDS allocated storage"
+  type        = number
+  default     = 20
+}
+
+variable "db_username" {
+  description = "RDS admin username"
+  type        = string
+  default     = "petclinic"
+}
