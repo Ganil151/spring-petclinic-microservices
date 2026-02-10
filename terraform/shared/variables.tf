7c2e7772a8853ab40a41f51a4225f0f5d44389d4
Ganil151 <ganilbatistyan@gmail.com>
2026-02-10 06:03:35 -0400

    refactor(spring-petclinic-microservices): Update variables.tf
    Changes summary:
    - Modified: 1 files
      • terraform/shared/variables.tf

--- terraform/shared/variables.tf
+++ terraform/shared/variables.tf
@@ -1,7 +1,7 @@
 variable "aws_region" {}
 variable "Environment" {}
 variable "bucket_name" {
-  
+  default = "petclinic-terraform-state-17a538b3"
 }
 variable "spms_vpc_cidr" {}
 variable "spms_subnet_cidr" {}
