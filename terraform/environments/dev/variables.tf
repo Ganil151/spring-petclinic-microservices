43307168048a25ae6765bcbad69ccf82f7fa7476
Ganil151 <ganilbatistyan@gmail.com>
2026-02-10 11:15:21 -0400

    refactor(spring-petclinic-microservices): Update variables.tf
    Changes summary:
    - Modified: 1 files
      • terraform/environments/dev/variables.tf

--- terraform/environments/dev/variables.tf
+++ terraform/environments/dev/variables.tf
@@ -21,7 +21,7 @@ variable "vpc_cidr" {
 variable "public_subnets_cidrs" {
   description = "Public subnets CIDR blocks"
   type        = list(string)
-  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+  default     = ["10.0.0.0/24", "10.0.2.0/24"]
 }
 
 variable "private_subnets_cidrs" {
