c6bb468f0e0aae65e1a37fa3cfae9bb9347b5008
Ganil151 <ganilbatistyan@gmail.com>
2026-02-10 16:15:42 -0400

    refactor(spring-petclinic-microservices): Update variables.tf
    Changes summary:
    - Modified: 1 files
      • terraform/modules/networking/alb/variables.tf

---
+++ terraform/modules/networking/alb/main.tf
@@ -0,0 +1,49 @@
+resource "aws_lb" "this" {
+  name               = "${var.project_name}-${var.environment}-alb"
+  internal           = var.internal
+  load_balancer_type = "application"
+  security_groups    = var.security_group_ids
+  subnets            = var.public_subnet_ids
+
+  enable_deletion_protection = var.enable_deletion_protection
+
+  tags = {
+    Name        = "${var.project_name}-${var.environment}-alb"
+    Environment = var.environment
+    Project     = var.project_name
+  }
+}
+
+# Default Target Group (Placeholder)
+resource "aws_lb_target_group" "default" {
+  name     = "${var.project_name}-${var.environment}-default-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = var.vpc_id
+
+  health_check {
+    path                = "/"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+
+  tags = {
+    Name        = "${var.project_name}-${var.environment}-default-tg"
+    Environment = var.environment
+    Project     = var.project_name
+  }
+}
+
+# Default HTTP Listener
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.this.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.default.arn
+  }
+}
