1e3327a57f7e8e704804586ee13848ed52c2297b
Ganil151 <ganilbatistyan@gmail.com>
2026-02-12 07:28:30 -0400

    refactor(spring-petclinic-microservices): Update main.tf
    Changes summary:
    - Modified: 1 files
      • terraform/modules/keys/main.tf

--- terraform/modules/keys/main.tf
+++ terraform/modules/keys/main.tf
@@ -8,4 +8,14 @@ resource "local_file" "spms_dev" {
   filename = "${path.module}/spms-dev.pem"
 }
 
+resource "aws_key_pair" "spms_dev" {
+  key_name   = "spms-dev"
+  public_key = local_file.spms_dev.content
+
+  tags = {
+    Name        = "spms-dev"
+    Environment = var.environment
+    Project     = var.project_name
+  }
+}
 
