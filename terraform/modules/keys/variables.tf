c3446b8c52b3b61e69881fc7132448eb4f1bcaed
Ganil151 <ganilbatistyan@gmail.com>
2026-02-17 09:18:54 -0400

    refactor(spring-petclinic-microservices): Update variables.tf
    Changes summary:
    - Modified: 1 files
      • terraform/modules/keys/variables.tf

--- terraform/modules/keys/variables.tf
+++ terraform/modules/keys/variables.tf
@@ -13,6 +13,12 @@ variable "key_name" {
   type        = string
 }
 
+variable "output_path" {
+  description = "The path where the .pem file should be written"
+  type        = string
+  default     = "."
+}
+
 variable "tags" {
   description = "Tags"
   type        = map(string)
