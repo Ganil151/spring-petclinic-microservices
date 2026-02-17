ce16cb212402afd9943bd7072ac6d38584eba56e
Ganil151 <ganilbatistyan@gmail.com>
2026-02-17 14:09:33 -0400

    refactor(spring-petclinic-microservices): Update jenkins_bootstrap.sh
    Changes summary:
    - Modified: 1 files
      • terraform/scripts/jenkins_bootstrap.sh

--- terraform/scripts/jenkins_bootstrap.sh
+++ terraform/scripts/jenkins_bootstrap.sh
@@ -12,16 +12,18 @@ echo "Updating system and installing Java 21..."
 sudo dnf update -y
 sudo dnf install fontconfig java-21-amazon-corretto-devel -y
 
+echo "Configure Java"
+JAVA_HOME="/usr/lib/jvm/java-21-amazon-corretto"
+echo "export JAVA_HOME=$JAVA_HOME" | sudo tee -a ~/.bashrc
+echo "export PATH=$PATH:$HOME/bin:$JAVA_HOME" | sudo tee -a ~/.bashrc
+
 # 3. Install Jenkins
 echo "Installing Jenkins..."
 sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 sudo dnf install jenkins -y
 
-echo "Configure Java"
-JAVA_HOME="/usr/lib/jvm/java-21-amazon-corretto"
-echo "export JAVA_HOME=$JAVA_HOME" | sudo tee -a ~/.bashrc
-echo "export PATH=$PATH:$HOME/bin:$JAVA_HOME" | sudo tee -a ~/.bashrc
+
 
 # 4. Install Component Tools
 echo "Installing Git, Docker, Python3, Ansible, jq..."
