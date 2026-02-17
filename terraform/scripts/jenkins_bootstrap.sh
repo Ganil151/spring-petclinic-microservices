3bba249df03685a04602ab995c87d2b25ff225be
Ganil151 <ganilbatistyan@gmail.com>
2026-02-17 06:34:22 -0400

    refactor(spring-petclinic-microservices): Update jenkins_bootstrap.sh
    Changes summary:
    - Modified: 1 files
      • terraform/scripts/jenkins_bootstrap.sh

--- terraform/scripts/jenkins_bootstrap.sh
+++ terraform/scripts/jenkins_bootstrap.sh
@@ -6,7 +6,7 @@ sudo hostnamectl set-hostname jenkins-master
 
 # Sleep for 60 seconds as requested to ensure instance is fully ready
 echo "Sleeping for 60 seconds..."
-sleep 60
+sleep 65
 
 # ==============================================================================
 # CONFIGURATION (Update these variables)
