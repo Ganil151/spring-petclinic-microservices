d7a39eb84de153329e8446573d0e77182c7f0f6d
Ganil151 <ganilbatistyan@gmail.com>
2026-02-18 13:46:02 -0400

    refactor(spring-petclinic-microservices): Update sonarqube_bootstrap.sh
    Changes summary:
    - Modified: 1 files
      • terraform/scripts/sonarqube_bootstrap.sh

--- terraform/scripts/sonarqube_bootstrap.sh
+++ terraform/scripts/sonarqube_bootstrap.sh
@@ -15,7 +15,7 @@ echo "Waiting for instance stabilization..."
 sleep 30
 
 # ─── 3. System Update & SSH Dependencies ─────────────────────────
-sudo dnf update -y
-sudo dnf install -y python3 python3-pip
+sudo yum update -y
+sudo yum install -y git python3 python3-pip
 
 echo "✅ Bootstrap complete. Ready for Ansible configuration."
