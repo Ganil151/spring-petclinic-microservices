221ddeff3d653c1f1b961b6014d9b365c878449f
Ganil151 <ganilbatistyan@gmail.com>
2026-02-19 19:19:44 -0400

    refactor(spring-petclinic-microservices): Update main.tf
    Changes summary:
    - Modified: 1 files
      • terraform/environments/dev/main.tf

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -33,6 +33,7 @@ module "sg" {
   project_name        = var.project_name
   environment         = var.environment
   vpc_id              = module.vpc.vpc_id
+  vpc_cidr            = var.vpc_cidr
   allowed_cidr_blocks = var.allowed_cidr_blocks
   ingress_ports       = var.ingress_ports
 }
