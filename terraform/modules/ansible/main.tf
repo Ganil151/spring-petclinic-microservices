9d8b906d50e7cf86ace6b2f42a9729bcffa3d0ed
Ganil151 <ganilbatistyan@gmail.com>
2026-02-20 17:03:19 -0400

    feat(spring-petclinic-microservices): Add workspace.json and 2 others
    Changes summary:
    - Modified: 1 files
      • .obsidian/workspace.json
    - Untracked: 2 files
      • terraform/modules/ansible/main.tf
      • terraform/modules/ansible/variables.tf

---
+++ terraform/modules/ansible/main.tf
@@ -0,0 +1,49 @@
+resource "local_file" "ansible_inventory" {
+  filename        = var.inventory_file_path
+  file_permission = "0600"
+
+  content = templatefile("${path.module}/templates/ansible_inventory.tftpl", {
+    jenkins_master_ip    = var.jenkins_master_ip
+    jenkins_master_priv  = var.jenkins_master_priv
+    worker_node_ips      = var.worker_node_ips
+    worker_node_priv_ips = var.worker_node_priv_ips
+    sonarqube_ip         = var.sonarqube_ip
+    sonarqube_priv       = var.sonarqube_priv
+    ssh_user             = var.ssh_user
+    ssh_key_file         = var.ssh_key_file
+    eks_cluster_name     = var.eks_cluster_name
+    aws_region           = var.aws_region
+    vpc_id               = var.vpc_id
+    account_id           = var.account_id
+    project_name         = var.project_name
+    env_name             = var.environment
+  })
+}
+
+resource "null_resource" "encrypt_inventory" {
+  count = var.environment == "prod" ? 1 : 0
+  
+  triggers = {
+    inventory_content = local_file.ansible_inventory.content
+  }
+
+  provisioner "local-exec" {
+    command = "ansible-vault encrypt ${var.inventory_file_path} --vault-password-file ../.vault_pass"
+  }
+
+  depends_on = [local_file.ansible_inventory]
+}
+
+resource "null_resource" "run_ansible" {
+  count = var.run_ansible ? 1 : 0
+
+  triggers = {
+    inventory_id = local_file.ansible_inventory.id
+  }
+
+  provisioner "local-exec" {
+    command = "cd ${var.ansible_working_dir} && ansible-playbook playbooks/site.yml"
+  }
+
+  depends_on = [local_file.ansible_inventory, null_resource.encrypt_inventory]
+}
