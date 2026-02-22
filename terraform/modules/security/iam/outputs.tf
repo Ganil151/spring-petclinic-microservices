d71fa3f7859bd8b85a1d7e921390c9b73123caba
Ganil151 <ganilbatistyan@gmail.com>
2026-02-22 16:24:06 -0400

    refactor(spring-petclinic-microservices): Update outputs.tf
    Changes summary:
    - Modified: 1 files
      • terraform/modules/security/iam/outputs.tf

--- terraform/modules/security/iam/outputs.tf
+++ terraform/modules/security/iam/outputs.tf
@@ -61,17 +61,17 @@ output "bastion_security_group_id" {
 output "all_security_group_ids" {
   description = "Map of all security group IDs"
   value = {
-    api_gateway    = aws_security_group.api_gateway.id
-    admin_server   = aws_security_group.admin_server.id
-    config_server  = aws_security_group.config_server.id
-    discovery      = aws_security_group.discovery_server.id
-    microservices  = aws_security_group.microservices.id
-    database       = aws_security_group.database.id
-    zipkin         = var.enable_zipkin ? aws_security_group.zipkin[0].id : null
-    grafana        = var.enable_grafana ? aws_security_group.grafana[0].id : null
-    prometheus     = var.enable_prometheus ? aws_security_group.prometheus[0].id : null
-    alb            = aws_security_group.alb.id
-    k8s_nodes      = aws_security_group.k8s_nodes.id
-    bastion        = aws_security_group.bastion.id
+    api_gateway   = aws_security_group.api_gateway.id
+    admin_server  = aws_security_group.admin_server.id
+    config_server = aws_security_group.config_server.id
+    discovery     = aws_security_group.discovery_server.id
+    microservices = aws_security_group.microservices.id
+    database      = aws_security_group.database.id
+    zipkin        = var.enable_zipkin ? aws_security_group.zipkin[0].id : null
+    grafana       = var.enable_grafana ? aws_security_group.grafana[0].id : null
+    prometheus    = var.enable_prometheus ? aws_security_group.prometheus[0].id : null
+    alb           = aws_security_group.alb.id
+    k8s_nodes     = aws_security_group.k8s_nodes.id
+    bastion       = aws_security_group.bastion.id
   }
 }
