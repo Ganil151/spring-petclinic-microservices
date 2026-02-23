fea26add26da9792328c1c22c3902c94c775193b
Ganil151 <ganilbatistyan@gmail.com>
2026-02-23 13:37:01 -0400

    refactor(spring-petclinic-microservices): Update main.tf
    Changes summary:
    - Modified: 1 files
      • terraform/modules/compute/k8s-node/main.tf

--- terraform/modules/compute/k8s-node/main.tf
+++ terraform/modules/compute/k8s-node/main.tf
@@ -1,10 +1,25 @@
+data "aws_ami" "amazon_linux_2" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
 resource "aws_instance" "k8s_node" {
-  ami           = var.ami.id
-  instance_type = var.instance_type
-  subnet_id     = var.subnet_ids[0]
+  ami                    = var.ami_id == "" ? data.aws_ami.amazon_linux_2.id : var.ami_id
+  instance_type          = var.instance_type
+  subnet_id              = var.subnet_ids[0]
+  vpc_security_group_ids = [] # We should probably pass this too
 
   tags = {
     Name = "${var.project_name}-node"
   }
-
 }
