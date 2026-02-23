b6382ac940ab11e32345b7a0439918f2807a4dee
Ganil151 <ganilbatistyan@gmail.com>
2026-02-23 13:35:37 -0400

    refactor(spring-petclinic-microservices): Update variables.tf
    Changes summary:
    - Modified: 1 files
      • terraform/modules/compute/k8s-node/variables.tf

--- terraform/modules/compute/k8s-node/variables.tf
+++ terraform/modules/compute/k8s-node/variables.tf
@@ -1,7 +1,26 @@
 variable "project_name" {
-
+  description = "Project name"
+  type        = string
 }
+
 variable "instance_type" {
-
+  description = "EC2 instance type"
+  type        = string
+  default     = "t3.medium"
 }
 
+variable "subnet_ids" {
+  description = "List of subnet IDs"
+  type        = list(string)
+}
+
+variable "vpc_id" {
+  description = "VPC ID"
+  type        = string
+}
+
+variable "ami_id" {
+  description = "AMI ID (optional)"
+  type        = string
+  default     = ""
+}
