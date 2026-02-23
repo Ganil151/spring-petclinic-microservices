acd4c07b74504adb3b7384b2020690bd5303b5e7
Ganil151 <ganilbatistyan@gmail.com>
2026-02-23 14:11:07 -0400

    refactor(spring-petclinic-microservices): Update workspace.json and 5 others
    Changes summary:
    - Modified: 3 files
      • .obsidian/workspace.json
      • terraform/live/dev/ec2-instances/plan.err
      • terraform/modules/compute/ec2-instances/main.tf
    - Untracked: 3 files
      • inputs.txt
      • terraform/live/dev/ec2-instances/plan2.err
      • vars.txt

--- terraform/modules/compute/ec2-instances/main.tf
+++ terraform/modules/compute/ec2-instances/main.tf
@@ -38,7 +38,7 @@ resource "aws_instance" "bastion" {
   }
 
   monitoring = var.enable_monitoring
-  user_data  = var.bastion_user_data
+  user_data_base64 = var.bastion_user_data
 
   tags = merge({
     Name        = "${var.project_name}-${var.environment}-${var.bastion_instance_name}"
@@ -92,7 +92,7 @@ resource "aws_instance" "jenkins" {
   }
 
   monitoring = var.enable_monitoring
-  user_data  = var.jenkins_user_data
+  user_data_base64 = var.jenkins_user_data
 
   tags = merge({
     Name        = "${var.project_name}-${var.environment}-${var.jenkins_instance_name}"
@@ -134,7 +134,7 @@ resource "aws_instance" "sonarqube" {
   }
 
   monitoring = var.enable_monitoring
-  user_data  = var.sonarqube_user_data
+  user_data_base64 = var.sonarqube_user_data
 
   lifecycle {
     create_before_destroy = true
@@ -181,7 +181,7 @@ resource "aws_instance" "worker_nodes" {
   }
 
   monitoring = var.enable_monitoring
-  user_data  = var.worker_user_data
+  user_data_base64 = var.worker_user_data
 
   tags = merge({
     Name        = "${var.project_name}-${var.environment}-${var.worker_instance_name}-${count.index + 1}"
