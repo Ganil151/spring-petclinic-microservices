3c1f2a953322321fd3b829ca9591bffda8c1d05e
Ganil151 <ganilbatistyan@gmail.com>
2026-02-24 16:15:32 -0400

    refactor(spring-petclinic-microservices): Update deployment-pipeline.groovy
    Changes summary:
    - Modified: 1 files
      • jenkins/pipelines/deployment-pipeline.groovy

--- jenkins/pipelines/deployment-pipeline.groovy
+++ jenkins/pipelines/deployment-pipeline.groovy
@@ -1,13 +1,17 @@
 // Modular Deployment Script
 // Called by the root Jenkinsfile stage('Containerize & Deploy')
 
-container('docker') {
-    echo "🐳 Building Docker Images for Microservices..."
-    sh "docker build -t spring-petclinic-api-gateway:latest ./spring-petclinic-api-gateway"
-    // sh "docker push ... "
+def call() {
+    container('docker') {
+        echo "🐳 Building Docker Images for Microservices..."
+        sh "docker build -t spring-petclinic-api-gateway:latest ./spring-petclinic-api-gateway"
+        // sh "docker push ... "
+    }
+
+    container('kubectl') {
+        echo "🚢 Deploying to EKS Cluster..."
+        sh "kubectl apply -k k8s/overlays/dev"
+    }
 }
 
-container('kubectl') {
-    echo "🚢 Deploying to EKS Cluster..."
-    sh "kubectl apply -k k8s/overlays/dev"
-}
+return this
